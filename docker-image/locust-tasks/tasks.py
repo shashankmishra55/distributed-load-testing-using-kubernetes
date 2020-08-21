141471ffa6b7be04d51a9a177c7912775fe1b9ea
shashank <shashank@2sgamin.com>
2020-08-21 15:40:34 +0530

    Changes

--- docker-image/locust-tasks/tasks.py
+++ docker-image/locust-tasks/tasks.py
@@ -23,19 +23,24 @@ from locust import HttpLocust, TaskSet, task
 
 class MetricsTaskSet(TaskSet):
     _deviceid = None
-
+    _token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZjNkMmRmMGViNzJkOWJjYjExZjk5NTgiLCJpYXQiOjE1OTc4NTA5NTN9.-GqMOayd4r29xKTf1PeHuvqGwLzupwCYN4kFfGxXQ6o"
     def on_start(self):
         self._deviceid = str(uuid.uuid4())
 
-    @task(1)
+    # @task(1)
+    # def login(self):
+    #     self.client.post(
+    #         '/login', {"deviceid": self._deviceid})
+
+     @task(1000)
     def login(self):
         self.client.post(
-            '/login', {"deviceid": self._deviceid})
+            '/auth?token=cCI6IkpXVCJ9.eyJfaWQiOiI1ZjNkMmRmMGViNzJkOWJjYjExZjk5NTgiLCJpYXQiOjE1OTc4NTA5NTN9.-GqMOayd4r29xKTf1PeHuvqGwLzupwCYN4kFfGxXQ6o', {})
 
-    @task(999)
-    def post_metrics(self):
-        self.client.post(
-            "/metrics", {"deviceid": self._deviceid, "timestamp": datetime.now()})
+    # @task(999)
+    # def post_metrics(self):
+    #     self.client.post(
+    #         "/metrics", {"deviceid": self._deviceid, "timestamp": datetime.now()})
 
 
 class MetricsLocust(HttpLocust):
